57239963ca891e18b4474269a7de30b64a935156
Davide Teixeira <davidpcu@hotmail.com>
2022-11-30 09:03:17 +0000

    Added Static Entities

---
+++ pacman/src/main/java/org/l11gr05/PowerPellets.java
@@ -0,0 +1,13 @@
+package org.l11gr05;
+
+public class PowerPellets extends Element{
+
+
+    public PowerPellets(Position position) {
+        super(position);
+    }
+
+    public PowerPellets(int x, int y){
+        super(x, y);
+    }
+}
