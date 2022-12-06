232f03f54c1c27eb39b24f5465eb70324fa27331
Emanuel Maia <tavanomanu@gmail.com>
2022-12-06 15:03:18 +0000

    some changes

--- pacman/src/main/java/org/l11gr05/Pacman.java
+++ pacman/src/main/java/org/l11gr05/Pacman.java
@@ -1,5 +1,8 @@
 package org.l11gr05;
 
+import org.l11gr05.arena.Element;
+import org.l11gr05.arena.Position;
+
 public class Pacman extends Element {
     public Pacman(Position position) {
         super(position);
@@ -8,6 +11,4 @@ public class Pacman extends Element {
     public Pacman() {
         super(new Position(5, 5));
     }
-
-
 }
