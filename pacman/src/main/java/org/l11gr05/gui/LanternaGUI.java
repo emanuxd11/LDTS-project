3facd561118093747d6d00423d94958735f1d410
Davide Teixeira <davidpcu@hotmail.com>
2022-12-12 13:04:16 +0000

    Changed the fontsize

--- pacman/src/main/java/org/l11gr05/gui/LanternaGUI.java
+++ pacman/src/main/java/org/l11gr05/gui/LanternaGUI.java
@@ -80,7 +80,7 @@ public class LanternaGUI implements GUI {
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         ge.registerFont(font);
 
-        Font loadedFont = font.deriveFont(Font.PLAIN, 25);
+        Font loadedFont = font.deriveFont(Font.PLAIN, 24);
         AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
         return fontConfig;
     }
